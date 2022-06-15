a8e58ee3a0b0a53b29ad94ac0dfcfdf331e69757
Sergey <ser_8904@mail.ru>
2022-06-15 15:14:20 +0300

    fix find_by!

--- db/migrate/20220610082108_add_admin_role.rb
+++ db/migrate/20220610082108_add_admin_role.rb
@@ -1,5 +1,5 @@
 class AddAdminRole < ActiveRecord::Migration[6.1]
   def change
-    User.find_by!(email: 'ser_8904@mail.ru')&.update(admin: true)
+    User.find_by(email: 'ser_8904@mail.ru')&.update(admin: true)
   end
 end
